f3100d1f832171c100f0b46d05c35591b8617446
Alex <166158936+AlexAlexChen@users.noreply.github.com>
2024-12-11 17:41:57 -0800

    Update contoller.gd

--- cardgame/scripts/contoller.gd
+++ cardgame/scripts/contoller.gd
@@ -3,3 +3,9 @@ class_name Controller
 extends Node
 
 #Parent class for player and enemy
+
+@export var score_card: ScoreCard = null
+@export var status_card: StatusCard = null
+@export var displayed_cards: CardDeck = null
+@export var card_deck: CardDeck = null
+@export var used_card_deck: CardDeck = null
