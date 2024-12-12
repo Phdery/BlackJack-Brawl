d72c463af2e88fc84a68e0c1b5cdb64825e65fce
Alex <166158936+AlexAlexChen@users.noreply.github.com>
2024-12-11 17:40:29 -0800

    Update player.gd

--- cardgame/scripts/player.gd
+++ cardgame/scripts/player.gd
@@ -1,20 +1,27 @@
 class_name Player
-extends Controller
+extends Controller 
 
-@onready var player_score_card: ScoreCard = $VBoxContainer/CenterContainer/PlayerScoreCard
-@onready var player_status_card: StatusCard = $VBoxContainer/CenterContainer/PlayerStatusCard
-@onready var player_displayed_cards: CardDeck = $VBoxContainer/CenterContainer2/PlayerDisplayedCardDeck
-@onready var player_card_deck: CardDeck = $VBoxContainer/CenterContainer2/PlayerCardDeck
-@onready var player_used_card_deck: CardDeck = $VBoxContainer/CenterContainer2/PlayerUsedCardDeck
+
 
 # Enum for card suits
 const Suit = GameGlobal.Suit
 var suit: Suit
 
 var is_stopped: bool = false  # Tracks if the player's turn is stopped
-var score_card = preload("res://scenes/score_card.tscn").instantiate()
-var basic_card = preload("res://scenes/basic_card.tscn")
 
+func _ready() -> void:
+	score_card = $VBoxContainer/CenterContainer/PlayerScoreCard
+	status_card = $VBoxContainer/CenterContainer/PlayerStatusCard
+	displayed_cards = $VBoxContainer/CenterContainer2/PlayerDisplayedCardDeck
+	card_deck = $VBoxContainer/CenterContainer2/PlayerCardDeck
+	used_card_deck = $VBoxContainer/CenterContainer2/PlayerUsedCardDeck
+	
+	modify_health(100.0)
+	suit = GameGlobal.chosen_suit
+	var suit_string: String = ""
+	suit_string = GameGlobal.suit_string(suit)
+	initialize_deck(suit_string)
+	
 # Initializes the player's deck based on the chosen suit
 func initialize_deck(suit: String) -> void:
 	
@@ -24,23 +31,14 @@ func initialize_deck(suit: String) -> void:
 	for score in scores:
 		var new_card = card_resource.instantiate() as BasicCard
 		new_card.custom_init(score, suit)
-		player_card_deck.add_card(new_card)
+		card_deck.add_card(new_card)
 
-	shuffle(player_card_deck.cards)
-
-
-func _ready() -> void:
-	modify_health(100.0)
-	suit = GameGlobal.chosen_suit
-	var suit_string: String = ""
-	suit_string = GameGlobal.suit_string(suit)
-	print("Card suit selected: ", suit_string)
-	initialize_deck(suit_string)
+	shuffle(card_deck.cards)
 
 # Modifies the player's health (damage or heal) and updates the status card
 func modify_health(amount: int) -> void:
-	player_status_card.update_hp(clamp(player_status_card.current_hp + amount, 0, player_status_card.max_hp))
-	if player_status_card.current_hp <= 0:
+	status_card.update_hp(clamp(status_card.current_hp + amount, 0, status_card.max_hp))
+	if status_card.current_hp <= 0:
 		_on_player_death()
 
 # Handles the player's death
@@ -50,36 +48,36 @@ func _on_player_death() -> void:
 
 # Moves all cards from the used deck back to the main card deck when it's empty
 func refill_card_deck() -> void:
-	if player_card_deck.is_empty():
-		for card in player_used_card_deck.cards:
-			player_card_deck.add_card(card)
-		player_used_card_deck.clear()
-		shuffle(player_card_deck.cards)
+	if card_deck.is_empty():
+		for card in used_card_deck.cards:
+			card_deck.add_card(card)
+		used_card_deck.clear()
+		shuffle(card_deck.cards)
 
 # Draws a random card from the player's card deck
 func generate_random_card() -> BasicCard:
-	if player_card_deck.cards.size() == 0:
+	if card_deck.cards.size() == 0:
 		return null  # Return null if no cards are left in the deck
 		
 	# Generate a random index within the range of available cards
-	var random_index = randi() % player_card_deck.cards.size()
+	var random_index = randi() % card_deck.cards.size()
 
 	# Get the card at the random index
-	var random_card = player_card_deck.cards[random_index]
+	var random_card = card_deck.cards[random_index]
 
 	# Remove the card from the deck to ensure it isn't drawn again
-	player_card_deck.cards.erase(random_card)
+	card_deck.cards.erase(random_card)
 
 	return random_card  # Return the selected card
 
 # Draws a random card from the deck, moves it to the displayed deck, and executes its mechanism
 func draw_and_execute_card() -> void:
-	if player_card_deck.is_empty():
+	if card_deck.is_empty():
 		refill_card_deck()
 	
-	var drawn_card = player_card_deck.generate_random_card()
+	var drawn_card = card_deck.generate_random_card()
 	if drawn_card:
-		player_displayed_cards.add_card(drawn_card)
+		displayed_cards.add_card(drawn_card)
 		execute_card_mechanism(drawn_card)
 
 # Executes the mechanism of the drawn card
@@ -89,13 +87,13 @@ func execute_card_mechanism(card: Card) -> void:
 # Updates scores for both player and enemy at the end of a turn
 func update_scores() -> void:
 	# Set player's score and max_score to default
-	player_score_card.update_score(0)
-	player_score_card.update_max(21)
+	score_card.update_score(0)
+	score_card.update_max(21)
 
 func move_displayed_cards_to_used() -> void:
 	# Move player's displayed cards to used deck
-	for card in player_displayed_cards.cards:
-		card.move_card_to(card, player_used_card_deck)
+	for card in displayed_cards.cards:
+		card.move_card_to(card, used_card_deck)
 		
 # Returns whether the player has stopped (for turn management)
 func has_stopped() -> bool:
@@ -119,3 +117,10 @@ func shuffle(deck: Array) -> void:
 
 # Signal emitted when the player dies
 signal player_died
+
+func move_card_animation(card:Card, from_card_deck: CardDeck, to_card_deck:CardDeck, delta) -> void:
+	var start_position = from_card_deck.position
+	var end_position = to_card_deck.position
+	var time = 1
+	var velocity = (start_position - end_position) / time
+
